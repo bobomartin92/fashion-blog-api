cc1a5a0ceb25cbf2358e6417111bf4f1e0c196e9
Bodiseowei Martin Tonweriyai <bmtonweriyai@gmail.com>
2022-12-07 22:22:39 +0100

    using java 11

--- src/main/java/dev/decagon/fashionblog/exception/ApiException.java
+++ src/main/java/dev/decagon/fashionblog/exception/ApiException.java
@@ -1,8 +1,14 @@
 package dev.decagon.fashionblog.exception;
 
+import lombok.Data;
 import org.springframework.http.HttpStatus;
 
 import java.time.ZonedDateTime;
 
-public record ApiException(String message, HttpStatus httpStatus, ZonedDateTime zonedDateTime) {
+@Data
+public class ApiException {
+    private final String message;
+    private Throwable throwable;
+    private final HttpStatus httpStatus;
+    private final ZonedDateTime zonedDateTime;
 }
